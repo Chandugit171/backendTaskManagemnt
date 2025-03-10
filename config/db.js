315ceee3ff0b9cbcd0743efd05961109440363b5
Chandugit171 <mr.chandugit171@gmail.com>
2025-03-10 20:52:52 +0530

    added the dmin

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const dbName = "admin"; // ✅ Define your database name here
+// const dbName = "admin"; // ✅ Define your database name here
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName,
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}/${dbName}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
