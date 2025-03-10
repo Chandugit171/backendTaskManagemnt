aed62791cbcea703244663f650688977ff2fc202
Chandugit171 <mr.chandugit171@gmail.com>
2025-03-10 20:08:19 +0530

    add missing dependency

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/routes");
 
